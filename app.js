47777212e1d74a537e7b158e7d3fc5f588724f1b
vasundhara <vasukakarla89@gmail.com>
2022-10-19 11:29:48 +0000

    Gadgets Page

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+app.get("/gadgets", (request, response) => {
+  response.sendFile("./gadgets.html", { root: __dirname });
+});
+module.exports = app;
